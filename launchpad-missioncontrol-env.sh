28dd043f6006a4b0912239e42593ad68c858d7f8
invinciblejai <mailjai.vardhan@gmail.com>
2017-12-23 12:28:35 +0530

    updates env
    Signed-off-by: invinciblejai <mailjai.vardhan@gmail.com>

---
+++ launchpad-missioncontrol-env.sh
@@ -0,0 +1,10 @@
+#!/bin/sh 
+
+export PATH=$PATH:~/openshift/
+export PATH=$PATH:~/apache-maven-3.3.9/bin
+export LAUNCHER_MISSIONCONTROL_SERVICE_HOST=localhost
+export LAUNCHER_MISSIONCONTROL_SERVICE_PORT=8080
+export WIT_URL=https://api.openshift.io
+export AUTH_URL=https://auth.openshift.io
+export KEYCLOAK_SAAS_URL=https://sso.openshift.io/
+export OPENSHIFT_API_URL=https://api.starter-us-east-2.openshift.com
